3b336c26d3cbb7aea15d0cd82f4c6d38f33ae73a
Vraj Patel <vrajp771@gmail.com>
2022-09-07 19:02:05 +0530

    modified

--- DSA.java
+++
@@ -1,7 +0,0 @@
-public class DSA {
-    public static void main(String[] args) {
-        final  a=1;
-
-
-    }
-}
--- mutant.java
+++ mutant.java
@@ -1,26 +1,27 @@
 import java.util.*;
 public class mutant{
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int t = scan.nextInt();
-        for(int i=0 ; i<t ; i++){
-            int n,k;
-            n = scan.nextInt();
-            k = scan.nextInt();
-            int a[] = new int[101];
-            for(int j=0 ; j<n ; j++){
-                a[j] = scan.nextInt();
-            }
-            for(int j=0 ; j<n ; j++){
-                a[j] = a[j] + k ;
-            }
-            int ans=0;
-            for(int j=0 ; j<n ; j++){
-                if(a[j] % 7 == 0){
-                    ans++;
+        try (Scanner scan = new Scanner(System.in)) {
+            int t = scan.nextInt();
+            for(int i=0 ; i<t ; i++){
+                int n,k;
+                n = scan.nextInt();
+                k = scan.nextInt();
+                int a[] = new int[101];
+                for(int j=0 ; j<n ; j++){
+                    a[j] = scan.nextInt();
                 }
+                for(int j=0 ; j<n ; j++){
+                    a[j] = a[j] + k ;
+                }
+                int ans=0;
+                for(int j=0 ; j<n ; j++){
+                    if(a[j] % 7 == 0){
+                        ans++;
+                    }
+                }
+                System.out.println(ans);
             }
-            System.out.println(ans);
         }
     }
 }
