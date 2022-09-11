51c364600b6fa14fd1a680d802336f289f1c4306
Vraj Patel <vrajp771@gmail.com>
2022-09-11 20:07:57 +0530

    Shortcut prime number

--- prime.java
+++ prime.java
@@ -2,25 +2,28 @@ import java.util.Scanner;
 
 public class prime {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int flag = 0;
-        if(a==1){
-            System.out.println("Given number is not prime");
-        }
-        else{
-            for(int i=2;i<a/2;i++){
-                if(a%i==0){
-                    flag = 1;
-                    break;
+        try (Scanner scan = new Scanner(System.in)) {
+            int a = scan.nextInt();
+            int flag = 0;
+            int c = 2;
+            if(a==1){
+                System.out.println("Given number is not prime");
+            }
+            else{
+                while(c*c <= a){
+                    if(a%c==0){
+                        flag = 1;
+                        break;
+                    }
+                    c++;
                 }
             }
-        }
-        if(flag == 1){
-            System.out.println("Given number is not prime number");
-        }
-        else{
-            System.out.println("Given number is prime number");
+            if(flag == 1){
+                System.out.println("Given number is not prime number");
+            }
+            else{
+                System.out.println("Given number is prime number");
+            }
         }
     }
 }
